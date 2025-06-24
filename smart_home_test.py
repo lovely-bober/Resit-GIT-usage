0e45882a7058754ddcc7a0ffc3b31d1a028c52e4
Christy-kwakkel <christy.kwakkel@student.nhlstenden.com>
2025-06-24 13:33:52 +0200

    added comments to explain the code

--- smart_home_test.py
+++ smart_home_test.py
@@ -1,9 +1,10 @@
 import requests
 
 domoticz_url = "http://127.0.0.1:8080/json.htm"
-idx = 4
+idx = 4 # id number in domoticz
 command = input("On or Off: ").strip().capitalize()
 
+# parameters for the switching on and off
 params = {
     "type": "command",
     "param": "switchlight",
@@ -11,9 +12,10 @@ params = {
     "switchcmd": command
 }
 
-auth = ('admin', 'domoticz') 
+auth = ('admin', 'domoticz') # username and password for authentication/login in
 
 response = requests.get(domoticz_url, params=params, auth=auth)
 
+# printing the response
 print("Status code:", response.status_code)
 print(response.json())
