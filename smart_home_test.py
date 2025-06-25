7c7f39beb07db8941c1c20c90c1f424b348378b6
Christy-kwakkel <christy.kwakkel@student.nhlstenden.com>
2025-06-25 11:32:57 +0200

    added the change color function

--- smart_home_test.py
+++ smart_home_test.py
@@ -16,6 +16,27 @@ auth = ('admin', 'domoticz') # username and password for authentication/login in
 
 response = requests.get(domoticz_url, params=params, auth=auth)
 
-# printing the response
-print("Status code:", response.status_code)
-print(response.json())
+def change_color(hue, saturation, brightness=100):
+    """
+    Change the color of Philips Hue lamp.
+    
+    Args:
+        hue (int): Color hue (0-360)
+        saturation (int): Color saturation (0-100)
+        brightness (int): Brightness level (0-100), default 100
+    """
+    params = {
+        "type": "command",
+        "param": "setcolbrightnessvalue",
+        "idx": idx,
+        "hue": int(hue),
+        "brightness": int(brightness),
+        "saturation": int(saturation),
+        "iswhite": "false"
+    }
+
+    response = requests.get(domoticz_url, params=params, auth=auth)
+    
+    print("Status code:", response.status_code)
+    print(response.json())
+
